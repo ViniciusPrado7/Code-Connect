652811eddb08d52a5f69d2bf788880245a8a1efa
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2025-10-07 14:56:42 -0300

    feat: creating the card details page

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
 import { Avatar } from "../Avatar";
-import styles from "./cardPost.module.css"
+import styles from "./cardPost.module.css";
+import Link from "next/link";
 
-export const CardPost = ({ post }) => {
+export const CardPost = ({ post, highlight }) => {
   return (
-    <article className={styles.card}>
-      <header className={styles.header}>
-        <figure className={styles.figure}>
-          <Image src={post.cover} alt={`Capa do post de titulo: ${post.title}`} width={438} height={133} />
-        </figure>
-      </header>
-      <section className={styles.body}>
-        <h2 className={styles.h2}>{post.title}</h2>
-        <p className={styles.p}>{post.body}</p>
-      </section>
-      <footer className={styles.footer}>
-        <Avatar imageSrc={post.author.avatar} name={post.author.username} />
-      </footer>
-    </article>
+    <Link href={`/posts/${post.slug}`} className={styles.link}>
+      <article className={styles.card} style={{width: highlight ? 993 : 486}}>
+        <header className={styles.header}>
+          <figure className={styles.figure} style={{height: highlight ? 300 : 133}}>
+            <Image
+              src={post.cover}
+              alt={`Capa do post de titulo: ${post.title}`}
+              width={438}
+              height={133}
+            />
+          </figure>
+        </header>
+        <section className={styles.body}>
+          <h2 className={styles.h2}>{post.title}</h2>
+          <p className={styles.p}>{post.body}</p>
+        </section>
+        <footer className={styles.footer}>
+          <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+        </footer>
+      </article>
+    </Link>
   );
 };
